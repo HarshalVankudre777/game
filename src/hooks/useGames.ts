2df04c253dfa1e489d57d6c8d081759176d53d3c
IamYourWizard01 <118318367+HarshalVankudre777@users.noreply.github.com>
2024-06-13 20:53:17 +0200

    feat: Add GameCards component for displaying game information in a card format

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
     id: number;
     name: string;
+    background_image: string;
   }
   
   interface FetchGames {
@@ -39,5 +40,4 @@ const useGames = () => {
           );
     
 }
-
 export default useGames;
