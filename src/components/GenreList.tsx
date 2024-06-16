4e47ccfe3db7e289b3a8fc4e0b8a805adda968b0
IamYourWizard01 <118318367+HarshalVankudre777@users.noreply.github.com>
2024-06-16 22:55:33 +0200

    refactor: Update GameGrid and GenreList components for improved data fetching

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,21 @@
+import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 export const GenreList = () => {
   const { data } = useGenres();
   return (
     <div>
-      <ul>
+      <List>
         {data.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+          <ListItem key={genre.id} paddingY={'5px'}>
+            <HStack>
+              <Image boxSize={'32px'} src = { getCroppedImageUrl(genre.image_background)} alt={genre.name} />
+              <Text size={'lg'}>{genre.name}</Text>
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </div>
   );
 };
