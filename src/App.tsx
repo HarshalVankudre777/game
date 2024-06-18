6c0abb2dc9d10cccdc06c870bc594efa77144788
IamYourWizard01 <118318367+HarshalVankudre777@users.noreply.github.com>
2024-06-18 18:31:07 +0200

    refactor: Update GameGrid component to use 4 columns instead of 5

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import { GenreList } from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 
 function App() {
-   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <Grid
@@ -15,7 +19,6 @@ function App() {
         base: `"nav " " main"`,
         lg: `"nav nav" "aside main"`,
       }}
-      
       templateColumns={{
         base: "1fr",
         lg: "300px 1fr",
@@ -26,13 +29,22 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList  selectedGenre={selectedGenre}  onSelectGenre={(genre) => setSelectedGenre(genre) } />
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre}/>
-        </GridItem>
+        <PlatformSelector
+          onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+          selectedPlatform={selectedPlatform}
+        />
+        <GameGrid
+          selectedGenre={selectedGenre}
+          selectedPlatform={selectedPlatform}
+        />
+      </GridItem>
     </Grid>
   );
 }
