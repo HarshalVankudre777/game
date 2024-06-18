49290272a82e1ca5eef8b20f9562004e9ba21663
IamYourWizard01 <118318367+HarshalVankudre777@users.noreply.github.com>
2024-06-18 22:34:04 +0200

    refactor: Update App component to use Flex instead of HStack for better layout control

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import { GenreList } from "./components/GenreList";
@@ -42,13 +42,15 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
+        <Flex  paddingLeft={2} marginBottom={5}>
           <PlatformSelector
             onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}
             selectedPlatform={gameQuery.platform}
           />
-          <SortSelector  sortOrder={gameQuery.sortOrder} onSelectSort={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
-          </HStack>
+          <Box marginLeft={5}>
+            <SortSelector  sortOrder={gameQuery.sortOrder} onSelectSort={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
+          </Box>
+          </Flex>
           <GameGrid
             gameQuery={gameQuery}
           />
