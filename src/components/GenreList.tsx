401f6c639806efb3546010c5bca673ade6d3c4a4
IamYourWizard01 <118318367+HarshalVankudre777@users.noreply.github.com>
2024-06-20 22:57:34 +0200

    refactor: Update GenreList component to include heading and improve image display

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,6 +5,7 @@ import {
   Image,
   Spinner,
   Button,
+  Heading,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -21,20 +22,25 @@ export const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (isLoading) return <Spinner />;
   return (
     <div>
+      <Heading fontSize={'2xl'} marginBottom={3}>Genres</Heading>
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY={"5px"}>
             <HStack>
               <Image
                 boxSize={"32px"}
+                borderRadius={8}
+                objectFit={"cover"}
                 src={getCroppedImageUrl(genre.image_background)}
-                alt={genre.name}
+               
               />
               <Button
                 fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 size={"lg"}
                 variant={"link"}
+                whiteSpace={"normal"}
+                textAlign={"left"}
               >
                 {genre.name}
               </Button>
